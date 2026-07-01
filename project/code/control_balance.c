2b242c6d6d8d1024f6b4b0c35f44e35eb1db534b
ThAce-code <1960918149@qq.com>
2026-07-01 01:38:28 -0700

    Block non-finite balance outputs

--- project/code/control_balance.c
+++ project/code/control_balance.c
@@ -42,6 +42,23 @@ static float control_balance_limit_abs(float value, float limit)
     return value;
 }
 
+static uint8 control_balance_is_finite(float value)
+{
+    if(value != value)
+    {
+        return APP_FALSE;
+    }
+    if(APP_BALANCE_FINITE_ABS_LIMIT < value)
+    {
+        return APP_FALSE;
+    }
+    if((-APP_BALANCE_FINITE_ABS_LIMIT) > value)
+    {
+        return APP_FALSE;
+    }
+    return APP_TRUE;
+}
+
 static void control_balance_reset_derivative(void)
 {
     control_balance_last_pitch_deg = 0.0f;
@@ -162,6 +179,9 @@ void control_balance_update(uint32 now_ms)
        (APP_FALSE == wheel->right_online) ||
        (APP_FALSE == chassis->enable) ||
        (APP_FALSE == dt_valid) ||
+       (APP_FALSE == control_balance_is_finite(imu->pitch)) ||
+       (APP_FALSE == control_balance_is_finite(pitch_rate_dps)) ||
+       (APP_FALSE == control_balance_is_finite(wheel_speed_rpm)) ||
        (APP_BALANCE_TEST_PITCH_LIMIT_DEG < control_balance_absf(imu->pitch)))
     {
         control_balance_diag.safety_blocked = APP_TRUE;
@@ -188,6 +208,17 @@ void control_balance_update(uint32 now_ms)
 
     output_left_rpm = chassis->left_base_rpm + balance_rpm;
     output_right_rpm = chassis->right_base_rpm + balance_rpm;
+
+    if((APP_FALSE == control_balance_is_finite(control_balance_wheel_pos_rev)) ||
+       (APP_FALSE == control_balance_is_finite(balance_rpm)) ||
+       (APP_FALSE == control_balance_is_finite(output_left_rpm)) ||
+       (APP_FALSE == control_balance_is_finite(output_right_rpm)))
+    {
+        control_balance_diag.safety_blocked = APP_TRUE;
+        control_balance_reset_motion_state();
+        control_balance_stop_output();
+        return;
+    }
 
     control_balance_diag.balance_rpm = balance_rpm;
     control_balance_diag.output_left_rpm = output_left_rpm;
@@ -225,7 +256,16 @@ void control_balance_set_gain(float pitch_kp, float pitch_rate_kd)
 
 void control_balance_set_full_gain(float pitch_kp, float pitch_rate_kd, float wheel_speed_ks, float wheel_pos_kp)
 {
-    if((0.0f > pitch_kp) || (0.0f > pitch_rate_kd))
+    if((0.0f > pitch_kp) ||
+       (0.0f > pitch_rate_kd) ||
+       (APP_FALSE == control_balance_is_finite(pitch_kp)) ||
+       (APP_FALSE == control_balance_is_finite(pitch_rate_kd)) ||
+       (APP_FALSE == control_balance_is_finite(wheel_speed_ks)) ||
+       (APP_FALSE == control_balance_is_finite(wheel_pos_kp)) ||
+       (APP_BALANCE_GAIN_ABS_LIMIT < control_balance_absf(pitch_kp)) ||
+       (APP_BALANCE_GAIN_ABS_LIMIT < control_balance_absf(pitch_rate_kd)) ||
+       (APP_BALANCE_GAIN_ABS_LIMIT < control_balance_absf(wheel_speed_ks)) ||
+       (APP_BALANCE_GAIN_ABS_LIMIT < control_balance_absf(wheel_pos_kp)))
     {
         return;
     }

dfddc239e226035b371843f6b4980998c5794e50
ThAce-code <1960918149@qq.com>
2026-07-01 04:36:51 -0700

    Add IMU timing telemetry

--- project/code/telemetry.c
+++ project/code/telemetry.c
@@ -7,6 +7,7 @@
 #include "app_config.h"
 #include "actuator_motor.h"
 #include "control_balance.h"
+#include "sensor_imu.h"
 
 void telemetry_init(void)
 {
@@ -20,6 +21,8 @@ void telemetry_update(uint32 now_ms)
     const motor_rpm_loop_diag_struct *rpm_diag;
 #if APP_TELEMETRY_BALANCE_ENABLE
     const balance_diag_struct *balance;
+    const imu_state_struct *imu;
+    uint32 imu_age_ms;
     float vofa_data[14];
 #else
     float vofa_data[8];
@@ -30,6 +33,8 @@ void telemetry_update(uint32 now_ms)
 
 #if APP_TELEMETRY_BALANCE_ENABLE
     balance = control_balance_get_diag();
+    imu = sensor_imu_get_state();
+    imu_age_ms = now_ms - imu->timestamp_ms;
     vofa_data[0] = (float)now_ms;
     vofa_data[1] = (float)balance->mode;
     vofa_data[2] = balance->pitch_deg;
@@ -42,8 +47,8 @@ void telemetry_update(uint32 now_ms)
     vofa_data[9] = rpm_diag->right_motor_rpm;
     vofa_data[10] = rpm_diag->left_duty;
     vofa_data[11] = rpm_diag->right_duty;
-    vofa_data[12] = balance->pitch_kp;
-    vofa_data[13] = balance->pitch_rate_kd;
+    vofa_data[12] = (float)imu_age_ms;
+    vofa_data[13] = (float)sensor_imu_get_int_count();
 #else
     vofa_data[0] = (float)now_ms;
     vofa_data[1] = (float)rpm_diag->mode;
